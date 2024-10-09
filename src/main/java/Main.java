357b34b56c667b930985d2bccebd8b557eb1a185
eckofox1981 <franklineck@pm.me>
2024-10-09 21:59:16 +0200

    Added Main class and started project

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("""Välj funktion:\r
+                        1. addera
+                        2. substrahera
+                        3. substrahera
+                        4.dividera
+                        5. avsluta
+                        """);
+        int choice = scanner.nextInt();
+        boolean running = true;
+        while (running) {
+            switch (choice){
+                case 1 -> add();
+                case 2 -> substract();
+                case 3 -> multiply();
+                case 4 -> divide();
+                case 5 -> running = false;
+            }
+        }
+    }
+}
