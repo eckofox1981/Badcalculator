a522d85e449815fcaf67cfbd66e6514b4e9120b2
Stellunia <kiwi_kanin@hotmail.com>
2024-10-10 10:23:52 +0200

    adderad addition

--- src/main/java/Addera.java
+++ src/main/java/Addera.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Addera {
+
+    public static void addition() {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Ange första talet: ");
+        double aAddition = userInput.nextDouble();
+
+        System.out.println("Ange andra talet: ");
+        double bAddition = userInput.nextDouble();
+
+        double cAddition = aAddition + bAddition;
+        System.out.println(aAddition + " + " + bAddition + " = " + cAddition);
+    }
 }
