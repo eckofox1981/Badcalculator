eb57f9d58322cd036006afffd9daf7e298d61159
eckofox1981 <franklineck@pm.me>
2024-10-10 10:25:20 +0200

    fixedsome bugss (I am an idiot)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("""
+
+        boolean running = true;
+        while (running) {
+            System.out.println("""
                         Välj funktion:
                         1. addera
                         2. substrahera
@@ -12,15 +15,19 @@ public class Main {
                         4. dividera
                         5. avsluta
                         """);
-        int choice = scanner.nextInt();
-        boolean running = true;
-        while (running) {
+            int choice = scanner.nextInt();
             switch (choice){
-                case 1 -> add();
-                case 2 -> Subtract.subtract();
-                case 3 -> TimFunction.multiply();
-                case 4 -> Dividera.divide();
-                case 5 -> running = false;
+                //case 1: add();
+                case 2: Subtract.subtract();
+                break;
+                case 3: TimFunction.multiply();
+                break;
+                case 4: Dividera.divide();
+                break;
+                case 5: running = false;
+                break;
+                default: System.out.println("Try again");
+                break;
             }
         }
     }
